b35459dfc1bf1ad065809bd5adef27596c1b1e0f
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-11-17 01:58:53 +0600

    added all

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Blog Post (in Bangla):
+1. What are some differences between interfaces and types in TypeScript?
+
+
+
+
+
+2. What is the use of the keyof keyword in TypeScript? Provide an example.
+
 
---
+++ solution.ts
@@ -0,0 +1,18 @@
+function formatValue (value: string| number|boolean ){
+
+if(typeof value==='string'){
+    return value.toUpperCase()
+
+}
+
+if(typeof value==='number'){
+    return value*10
+}
+
+if(typeof value==='boolean'){
+    return !value
+}
+
+}
+
+
