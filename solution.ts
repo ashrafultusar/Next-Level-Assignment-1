485687388dd3ef4c5ef985005d1e96e71c9b0794
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-11-18 01:45:20 +0600

    added problem 2,3,4

--- solution.ts
+++ solution.ts
@@ -1,18 +1,73 @@
-function formatValue (value: string| number|boolean ){
-
-if(typeof value==='string'){
-    return value.toUpperCase()
-
+function formatValue(value: string | number | boolean) {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else if (typeof value === "boolean") {
+    return !value;
+  }
 }
 
-if(typeof value==='number'){
-    return value*10
+// console.log(formatValue('hello'));
+// console.log(formatValue(5));
+// console.log(formatValue(true));
+
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
+
+  throw new Error("Invalid Input");
 }
 
-if(typeof value==='boolean'){
-    return !value
+// console.log(getLength('typescript'));
+// console.log(getLength([10, 20, 30, 40]));
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}';`;
+  }
 }
 
+// const person1 = new Person("John Doe", 30);
+// console.log(person1.getDetails());
+
+// const person2 = new Person("Alice", 25);
+// console.log(person2.getDetails());
+
+
+
+
+
+type Item = {
+  title: string;
+  rating: number;
+};
+
+function filterByRating(item: Item[]): Item[] {
+  return item.filter((item) => item.rating >= 4);
 }
+
+
+
+const books = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
+
+// console.log(filterByRating(books));
+
+
 
 
