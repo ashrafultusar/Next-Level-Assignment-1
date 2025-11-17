2fb7eab6eddbf1b3c8595cf41954d525471724a5
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-11-18 02:08:19 +0600

    added problem 7

--- solution.ts
+++ solution.ts
@@ -96,8 +96,6 @@ function printBookDetails(book: Book): void {
   );
 }
 
-
-
 const myBook: Book = {
   title: 'The Great Gatsby',
   author: 'F. Scott Fitzgerald',
@@ -105,4 +103,34 @@ const myBook: Book = {
   isAvailable: true,
 };
 
-printBookDetails(myBook);
+// printBookDetails(myBook);
+
+
+
+function getUniqueValues (arr1: (number | string)[], arr2: (number | string)[]): (number | string)[] {
+  const result: (number | string)[] = [];
+  const combined = [...arr1, ...arr2]; // just to loop once
+
+  for (let i = 0; i < combined.length; i++) {
+    let isUnique = true;
+    for (let j = 0; j < result.length; j++) {
+      if (combined[i] === result[j]) {
+        isUnique = false;
+        break;
+      }
+    }
+    if (isUnique) {
+      result.push(combined[i]);
+    }
+  }
+
+  return result;
+}
+
+
+const array1 = [1, 2, 3, 4, 5,8];
+const array2 = [3, 4, 5, 6, 7,7,7,8,8];
+
+console.log(getUniqueValues(array1, array2)); 
+
+
